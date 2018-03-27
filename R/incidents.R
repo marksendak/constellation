74f3b631e6de010ef2ea6984cdb4e9300338b2af
Mark Sendak <mark.sendak@duke.edu>
2018-03-26 21:29:53 -0400

    - update version, incidents() tests to take less time, and links in vignette

--- R/incidents.R
+++ R/incidents.R
@@ -5,14 +5,14 @@
 #' The user must specify the number of hours over which events are considered
 #'  to be the same episode, the time stamp variable, and an optional variable
 #'  to group episodes. This function was motivated by examples where there may
-#'  be multiple observations of the same illness episode combined with 
+#'  be multiple observations of the same illness episode combined with
 #'  observations of distinct illness episodes and there is a need to
 #'  distinguish between episodes. This function assumes that the duration of
 #'  an episode is non-zero. If every non-equal instant is a distinct episode,
 #'  there is no need to use this function.
 #' Two ways to distinguish between episodes with non-zero duration over time
 #'  are: (1) bucket observations over a pre-fixed time frame that is applied to
-#'  all observation; (2) bucket observations over a fixed window of time. An 
+#'  all observation; (2) bucket observations over a fixed window of time. An
 #'  example of (1) is considering observations in the same month to be the same
 #'  episode and observations in different months to be distinct episodes. The
 #'  incident() function addresses (2) by specifying a time window and
@@ -20,15 +20,15 @@
 #'  a 'join_key' variable (person, encounter, etc.) to group episodes.
 #'  The 'window_hours' argument serves as the lower bound to separate
 #'  observations that are considered the same episode versus distinct episodes.
-#'  
+#'
 #' @param data A time series data frame that includes the columns 'join_key'
 #'  and 'time_var'
-#' @param window_hours A numeric value specifying the number of hours to 
+#' @param window_hours A numeric value specifying the number of hours to
 #'  separate contiguous episodes and distinct episodes
 #' @param time_var A string name of the time stamp column in the time series
 #'  data frame
 #' @param join_key An optional string name of the column to group observations
-#'  
+#'
 #' @return A data.frame, data.table with the time stamps of distinct, incident
 #'  episodes separated by at least 'window_hours'
 #'
@@ -53,18 +53,18 @@
 #'   format = "%Y-%m-%dT%H:%M:%SZ", tz = "UTC")]
 #'
 #' # Identify systolic blood pressure measurements for each patient that are
-#'  # separated by at least 4 hours
-#' incidents(systolic_bp, window_hours = 4, join_key = "PAT_ID",
+#'  # separated by at least 24 hours
+#' incidents(systolic_bp, window_hours = 24, join_key = "PAT_ID",
 #'  time_var = "RECORDED_TIME")
 #'
 #' # Identify systolic blood pressure measurements that are separated by at
-#'  # least 4 hours
-#' incidents(systolic_bp, window_hours = 4, time_var = "RECORDED_TIME")
+#'  # least 24 hours
+#' incidents(systolic_bp, window_hours = 24, time_var = "RECORDED_TIME")
 #'
 #' @export
 
 incidents <- function(data, window_hours, time_var, join_key = NULL) {
-  
+
   ########## Error handling ---------------------------------------------------
   # Missing arguments
   if (missing(data)) stop("Need to pass data frame")
@@ -96,7 +96,7 @@ incidents <- function(data, window_hours, time_var, join_key = NULL) {
 
   # Drop duplicate timestamps
   data <- unique(data)
-  
+
   # Build incident df
   incident <- data.table(NULL)
 
@@ -106,7 +106,7 @@ incidents <- function(data, window_hours, time_var, join_key = NULL) {
   } else {
     setkeyv(data, c(join_key, time_var))
   }
-  
+
   # Loop through events until gone through all events
   while(nrow(data) > 0) {
     # Empty placeholders for new variables
@@ -116,17 +116,17 @@ incidents <- function(data, window_hours, time_var, join_key = NULL) {
     # Drop events solely by timestamp
     if (missing(join_key)) {
       # Add first event to incident table
-      incident <- rbind(incident, data[1])  
+      incident <- rbind(incident, data[1])
 
       # Remove observations within window_hours from data
-      data <- data[, KEEP := get(time_var) >= (get(time_var)[1] + 
+      data <- data[, KEEP := get(time_var) >= (get(time_var)[1] +
         window_hours*60*60)][KEEP == "TRUE"][, KEEP := NULL]
     } else {
       # Add first event by join_key to incident table
       incident <- rbind(incident, data[, .SD[1], by = join_key])
 
       # Remove observations within window_hours from data by join_key
-      data <- data[, KEEP := get(time_var) >= (get(time_var)[1] + 
+      data <- data[, KEEP := get(time_var) >= (get(time_var)[1] +
         window_hours*60*60), by = join_key][KEEP == "TRUE"][, KEEP := NULL]
     }
   }
@@ -140,7 +140,7 @@ incidents <- function(data, window_hours, time_var, join_key = NULL) {
 
   # Drop duplicates
   incident <- unique(incident)
-  
+
   # return incident events
   return(incident)
 }
